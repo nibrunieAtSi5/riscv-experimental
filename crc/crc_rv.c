110576b724641b99863865bee8561315702178ea
Nicolas Brunie <nibrunie@gmail.com>
2026-09-06 09:30:20 -0700

    Improving rv_crc32_le_vector_clmul_fold with pre pointer alignement to 8-byte boundaries such that we can use 64-bit element vector load and save some vtype changes.

--- crc/crc_rv.c
+++ crc/crc_rv.c
@@ -321,19 +321,23 @@ uint32_t rv_crc32_le_vector_clmul_fold(uint32_t crc, unsigned char const *p, siz
             const uint64_t rmRev = 0x9ba54c6f00000000ull;
             const uint64_t r64Rev = 0xb8bc676500000000ull; 
             vuint64m1_t acc = __riscv_vmv_v_x_u64m1(0, 2);
-            if ((len % 8) != 0) {
-                  vuint8m1_t byte_data = __riscv_vle8_v_u8m1(p, len);
+            if (((size_t) p & 7) != 0) {
+                  size_t p_align_len = (size_t) p & 7;
+                  vuint8m1_t byte_data = __riscv_vle8_v_u8m1(p, p_align_len);
                   vuint64m1_t data = __riscv_vreinterpret_v_u8m1_u64m1(byte_data);
-                  size_t align_shift = 8 * (8 - len);
+                  size_t align_shift = 8 * (8 - p_align_len);
                   data = __riscv_vsll_vx_u64m1(data, align_shift, 1);
                   // FIXME: this is brittle as we need elements > 0 to be zero in zero (undisturbed compared to init)
                   acc = crc32_le_clmul64_v2(data);
-                  p += (len % 8);
+                  p += p_align_len;
+                  len -= p_align_len;
             }
 
             for (; len >= 32; len -= 16, p += 16) {
-                  vuint8m1_t byte_data = __riscv_vle8_v_u8m1(p, 16);
-                  vuint64m1_t data = __riscv_vreinterpret_v_u8m1_u64m1(byte_data);
+                  // since we have aligned p to a 8-byte boundary, we can safely load 64-bit elements
+                  // for the message (this should not be too slow / trap on uarchs which do not support mis-aligned
+                  // accesses natively). This saves somes vsetvli change.
+                  vuint64m1_t data = __riscv_vle64_v_u64m1((const unsigned long int *) p, 2);
                   data = __riscv_vxor_vv_u64m1(acc, data, 2);
                   vuint64m1_t folded_rem_hi = __riscv_vclmulh_vx_u64m1(data, rmRev, 2);
                   vuint64m1_t folded_rem_lo = __riscv_vclmul_vx_u64m1(data, rmRev, 2);
@@ -342,8 +346,7 @@ uint32_t rv_crc32_le_vector_clmul_fold(uint32_t crc, unsigned char const *p, siz
                   acc = __riscv_vxor_vv_u64m1(lo_rem, folded_rem_hi, 2);
             }
             {
-                  vuint8m1_t byte_data = __riscv_vle8_v_u8m1(p, 16);
-                  vuint64m1_t data = __riscv_vreinterpret_v_u8m1_u64m1(byte_data);
+                  vuint64m1_t data = __riscv_vle64_v_u64m1((const unsigned long int *) p, 2);
                   data = __riscv_vxor_vv_u64m1(acc, data, 2);
                   vuint64m1_t acc_hi = data;
                   vuint64m1_t acc_lo = __riscv_vslidedown_vx_u64m1(data, 1, 1);
@@ -359,6 +362,20 @@ uint32_t rv_crc32_le_vector_clmul_fold(uint32_t crc, unsigned char const *p, siz
                   vuint64m1_t data = __riscv_vreinterpret_v_u8m1_u64m1(byte_data);
                   data = __riscv_vxor_vv_u64m1(acc, data, 1);
                   acc = crc32_le_clmul64_v2(data);
+            }
+            // Because the pointer alignment is done initially (before the main loop),
+            // we need to handle the non multiple of 8 size in the epilog.
+            if (len != 0) {
+                  vuint8m1_t byte_data = __riscv_vle8_v_u8m1(p, len);
+                  vuint64m1_t data = __riscv_vreinterpret_v_u8m1_u64m1(byte_data);
+                  size_t align_shift = 8 * (8 - len);
+                  data = __riscv_vsll_vx_u64m1(data, align_shift, 1);
+                  vuint64m1_t crc_hi = __riscv_vsll_vx_u64m1(acc, align_shift, 1); 
+                  vuint64m1_t crc_lo = __riscv_vsrl_vx_u64m1(acc, 64 - align_shift, 1); 
+                  // FIXME: this is brittle as we need elements > 0 to be zero in zero (undisturbed compared to init)
+                  data = __riscv_vxor_vv_u64m1(data, crc_hi, 1); 
+                  acc = crc32_le_clmul64_v2(data);
+                  acc = __riscv_vxor_vv_u64m1(crc_lo, acc, 1); 
             }
 
             uint64_t crc = __riscv_vmv_x_s_u64m1_u64(acc);
